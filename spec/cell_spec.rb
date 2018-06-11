e8259065a216437191d0bb4ee4394be62cc762d7
Krishan <krishanchopra30@yahoo.com>
2018-06-11 14:31:43 -0700

    Write a failing test for cell that can die

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -11,8 +11,9 @@ describe "A cell" do
     expect(cell.alive).to be_truthy
   end
 
-  xit 'can die' do
-
+  it 'can die' do
+    cell = Cell.new
+    expect(cell.death).to exist
   end
 
   xit 'has neighbors, which is initially an empty array' do
